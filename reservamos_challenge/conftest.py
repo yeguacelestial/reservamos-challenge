80ef87050271a0b39fe07fefe35646d9dff628a3
Carlos Nava <yeguacelestial@gmail.com>
2024-03-08 15:04:25 -0600

    First commit

---
+++ reservamos_challenge/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from reservamos_challenge.users.models import User
+from reservamos_challenge.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture()
+def user(db) -> User:
+    return UserFactory()
