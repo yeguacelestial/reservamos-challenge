35aadc2b004a26f21d184b022a2123a6a197a6cd
Carlos Nava <yeguacelestial@gmail.com>
2024-03-11 17:56:34 -0600

    :construction: Refactor: get weather bulk

--- reservamos_challenge/forecasts/api/views.py
+++ reservamos_challenge/forecasts/api/views.py
@@ -7,42 +7,55 @@ from utils import reservamos
 class ForecastViewSet(viewsets.ViewSet):
     def list(self, request):
         # TODO: use a serializer
-        # 1. Read city
         city = request.query_params.get("city", None)
 
         response_data = {}
         if city:
             response_data = {"message": "City: " + city}
 
-            # 2. Request city info on Reservamos API
+            # Request city info on Reservamos API
             places = reservamos.get_places(city)
 
-            # NOTE: This is very slow
-            # 3. For each city:
+            latitudes = []
+            longitudes = []
+
+            # For each city:
             for place in places:
                 latitude = place.get("lat", "")
                 longitude = place.get("long", "")
 
-                #   3.2 Request weather info for the next 7 days, from OpenWeather API
-                place_weather = openweather.get_weather(
-                    latitude=latitude,
-                    longitude=longitude,
-                )
+                latitudes.append(latitude)
+                longitudes.append(longitude)
 
-                print("[D]", place_weather)
+            # Make a single call to OpenWeatherMap for all places
+            all_weather = openweather.get_weather_bulk(latitudes, longitudes)
 
-                #   3.3 Add weather info to the city properties
-                daily = place_weather.get("daily", [])
-                place["weather_forecast"] = {}
-                for day in daily:
-                    place["weather_forecast"][day["dt"]] = {
-                        "min_temp": day["temp"]["min"],
-                        "max_temp": day["temp"]["max"],
-                    }
+            for i, place in enumerate(places):
+                latitude = place.get("lat", "")
+                longitude = place.get("long", "")
 
-            # 4. Return cities names with min and max weather for the next 7 days
-            response_data = places
+                # Ensure all_weather has data and has the same length as places
+                if i < len(all_weather):
+                    place_weather = all_weather[i]
+                else:
+                    # Handle the case where all_weather doesn't have enough data
+                    place_weather = {}
+
+                daily_weather = place_weather.get("daily", [])
+
+                if daily_weather:
+                    # Simplify the data to just min/max temp for each day
+                    weather_forecast = {}
+                    for day in daily_weather:
+                        weather_forecast[day["dt"]] = {
+                            "min_temp": day["temp"]["min"],
+                            "max_temp": day["temp"]["max"],
+                        }
+                    place["weather_forecast"] = weather_forecast
+
+            response_data["places"] = places
+
         else:
-            response_data = {"error": "City was not received"}
+            response_data["error"] = "City was not received"
 
         return Response(response_data, status=200)
