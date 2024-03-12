ea85dbe3e7b5ef3d5320fe66f58bbb1719ba3465
Carlos Nava <yeguacelestial@gmail.com>
2024-03-12 12:06:23 -0600

    Refactor viewset

--- reservamos_challenge/forecasts/api/views.py
+++ reservamos_challenge/forecasts/api/views.py
@@ -6,7 +6,6 @@ from utils import reservamos
 
 class ForecastViewSet(viewsets.ViewSet):
     def list(self, request):
-        # TODO: use a serializer
         city = request.query_params.get("city", None)
 
         response_data = {}
@@ -16,36 +15,17 @@ class ForecastViewSet(viewsets.ViewSet):
             # Request city info on Reservamos API
             places = reservamos.get_places(city)
 
-            latitudes = []
-            longitudes = []
-
             # For each city:
             for place in places:
                 latitude = place.get("lat", "")
                 longitude = place.get("long", "")
 
-                latitudes.append(latitude)
-                longitudes.append(longitude)
-
-            # Make a single call to OpenWeatherMap for all places
-            all_weather = openweather.get_weather_bulk(latitudes, longitudes)
-
-            for i, place in enumerate(places):
-                latitude = place.get("lat", "")
-                longitude = place.get("long", "")
-
-                # Ensure all_weather has data and has the same length as places
-                if i < len(all_weather):
-                    place_weather = all_weather[i]
-                else:
-                    # Handle the case where all_weather doesn't have enough data
-                    place_weather = {}
-
+                place_weather = openweather.get_weather(latitude, longitude)
                 daily_weather = place_weather.get("daily", [])
 
                 if daily_weather:
-                    # Simplify the data to just min/max temp for each day
                     weather_forecast = {}
+
                     for day in daily_weather:
                         weather_forecast[day["dt"]] = {
                             "min_temp": day["temp"]["min"],
--- reservamos_challenge/utils/openweather.py
+++ reservamos_challenge/utils/openweather.py
@@ -10,23 +10,3 @@ def get_weather(latitude: str, longitude: str):
     response = json.loads(response.text)
 
     return response
-
-
-def get_weather_bulk(latitudes, longitudes):
-    endpoint = "https://api.openweathermap.org/data/2.5/onecall"
-    api_key = settings.OPENWEATHER_API_KEY
-    params = {
-        "lat": ",".join(map(str, latitudes)),
-        "lon": ",".join(map(str, longitudes)),
-        "appid": api_key,
-    }
-
-    response = requests.get(endpoint, params=params)
-    response_data = json.loads(response.text)
-
-    print("[D] openweather.get_weather_bulk: ", response_data)
-    # Ensure the response is a list, if not, return an empty list
-    if not isinstance(response_data, list):
-        return []
-
-    return response_data
