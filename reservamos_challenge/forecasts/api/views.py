9c16f37a81d836ad387cb7f54bc5be560b164a16
Carlos Nava <yeguacelestial@gmail.com>
2024-03-12 12:17:26 -0600

    Add thread pool executor

--- reservamos_challenge/forecasts/api/views.py
+++ reservamos_challenge/forecasts/api/views.py
@@ -1,3 +1,4 @@
+from concurrent.futures import ThreadPoolExecutor
 from rest_framework import viewsets
 from rest_framework.response import Response
 from utils import openweather
@@ -10,30 +11,35 @@ class ForecastViewSet(viewsets.ViewSet):
 
         response_data = {}
         if city:
-            response_data = {"message": "City: " + city}
-
             # Request city info on Reservamos API
             places = reservamos.get_places(city)
 
-            # For each city:
-            for place in places:
-                latitude = place.get("lat", "")
-                longitude = place.get("long", "")
+            with ThreadPoolExecutor() as executor:
+                # For each city, create a task to fetch weather info:
+                future_weather = {
+                    executor.submit(
+                        openweather.get_weather,
+                        place.get("lat", ""),
+                        place.get("long", ""),
+                    ): place
+                    for place in places
+                }
 
-                place_weather = openweather.get_weather(latitude, longitude)
-                daily_weather = place_weather.get("daily", [])
+                for future in future_weather:
+                    place = future_weather[future]
+                    place_weather = future.result()
 
-                if daily_weather:
-                    weather_forecast = {}
+                    if "daily" in place_weather:
+                        weather_forecast = {}
 
-                    for day in daily_weather:
-                        weather_forecast[day["dt"]] = {
-                            "min_temp": day["temp"]["min"],
-                            "max_temp": day["temp"]["max"],
-                        }
-                    place["weather_forecast"] = weather_forecast
+                        for day in place_weather["daily"]:
+                            weather_forecast[day["dt"]] = {
+                                "min_temp": day["temp"]["min"],
+                                "max_temp": day["temp"]["max"],
+                            }
+                        place["weather_forecast"] = weather_forecast
 
-            response_data["places"] = places
+                response_data["places"] = places
 
         else:
             response_data["error"] = "City was not received"
